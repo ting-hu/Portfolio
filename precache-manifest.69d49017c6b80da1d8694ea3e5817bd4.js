c74812946ac0ab681c52c098d75fdb35fd385ab8
ting-hu <huxu@uwplatt.edu>
2022-12-02 14:39:09 -0600

    Updates

---
+++ precache-manifest.69d49017c6b80da1d8694ea3e5817bd4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bbabb5b7a11f5f64c0b1520e18d5bc0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7f1e0b1e9358972387e1",
+    "url": "./static/css/2.5bad6fff.chunk.css"
+  },
+  {
+    "revision": "c2fb4a567cc28fd46e70",
+    "url": "./static/css/main.458a4424.chunk.css"
+  },
+  {
+    "revision": "7f1e0b1e9358972387e1",
+    "url": "./static/js/2.c7a32c90.chunk.js"
+  },
+  {
+    "revision": "45dd6a8eb784f4bfc84b6e54cbce993a",
+    "url": "./static/js/2.c7a32c90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2fb4a567cc28fd46e70",
+    "url": "./static/js/main.83ce8c89.chunk.js"
+  },
+  {
+    "revision": "b3287891d2c1f1cb6915",
+    "url": "./static/js/runtime-main.590b0372.js"
+  },
+  {
+    "revision": "21e9a161a1b942e7e222abdc41730755",
+    "url": "./static/media/Resume.21e9a161.pdf"
+  }
+]);
